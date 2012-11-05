457397374aa2efa8c38fe07c941fa001b1f4838f
Douglas Mak <github@douglasmak.com>
2012-11-05 15:39:23 -0800

    Added sort links, and functionality for submit search on enter

--- index.php
+++ index.php
@@ -6,21 +6,26 @@ include 'search.php';
 	<head>
 		<title>JobLube</title>
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/joblube.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container">
 	  
 	  		<center>
 				<h1>Welcome to JobLube!</h1><br>
-				
-				<div>
+				<div id="search">
 					<input type="text" id="description" class="input-large" placeholder="Description / Keywords" name="description" value="<?php if(isset($_GET['description'])) { echo $_GET['description']; }?>">
 					<input type="text" id="location" class="input-large" placeholder="Zip Code" name="location" value="<?php if(isset($_GET['location'])) { echo $_GET['location']; }?>">
-					<button type="submit" class="btn">Search</button>
+					<button type="submit" class="btn" id='search-button'>Search</button>
 				</div>
 			</center>
 			
 			<center><div id="loader" style="display:none;margin-top:175px;background: url(img/loading.gif) no-repeat center center; width: 175px;height: 175px;"></div></center>
+			<div id="sort">
+				Sort by:
+				<a href='#'>date</a>
+				<a href='#'>relevance</a>
+			</div>	
 			<div id="results"></div>
 			
 			
@@ -32,9 +37,22 @@ include 'search.php';
 		
 		
 		<script>
-			
-			$(".btn").click(function() 
-			{		
+			var x = 0
+			$("#search input[type=text]").keyup(function(event){
+			    if(event.keyCode == 13){
+			    	$("#search-button").click()
+			    }
+			});
+
+			$("#search-button").click(function() 
+			{	
+				$("#sort").hide();	
+
+				if( $("#description").val() == '' ) {
+					// We can do client side checking here, be sure to check on server side too.
+					return
+				}
+
 				$("#loader").show();
 				$('#results').html(""); 	
 				var description = $("input#description").val();
@@ -47,10 +65,10 @@ include 'search.php';
 				  	data: dataString,  
 				  	success: function(data) {  
 					  	$("#loader").hide();
+					  	$("#sort").show();
 						$('#results').html(data); 
-						
 				  	}  
-				}); 
+				});
 			});
 		</script>
 	</body>
--- search.php
+++ search.php
@@ -11,10 +11,11 @@ if (isset($_GET["location"]) && isset($_GET["description"])) {
     //
     // Any sorting can be done here
     //
-    //sort by date
+    // sort by date
 	usort($JOBS, function($a, $b) {
 			return $a->getDate() - $b->getDate();
 	});
+	
 	echo '<table class="table table-striped">
 			<thead>
 			  <tr>
