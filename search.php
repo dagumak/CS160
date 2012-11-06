f4802af189b9e50d9c80353799af776fabc11fe1
philgebhardt <gebhardt.phil@gmail.com>
2012-11-05 21:03:12 -0800

    Added sort by relevance function to switch case.

--- search.php
+++ search.php
@@ -1,12 +1,20 @@
 <?php
 require_once 'scrapers/monster/MonsterScraper.php';
 
+//Array for holding keyword tokens from description input
+$KEYWORDS = array();
+
 if (isset($_GET["location"]) && isset($_GET["description"])) {
     $monster_scraper = new MonsterScraper();
     $JOBS = array();
+	
+	//Get keyword tokens from description input
+	$KEYWORDS = explode(" ", $_GET["description"]);
+	
     //If there are any results returned from scraping monster, collect them.
     if ($monster_results = $monster_scraper->scrape_monster($_GET["location"], $_GET["description"])) {
         $JOBS += $monster_results;
+		
     }
 
 
@@ -22,7 +30,24 @@ if (isset($_GET["location"]) && isset($_GET["description"])) {
 				);
 				break;
 			case 'relevance':
-				// Run function to sort the data by relevance and re-set the variable 
+				// Run function to sort the data by relevance and re-set the variable
+				// sort by relevance
+				usort($JOBS,
+					function($a, $b) {
+						GLOBAL $KEYWORDS;
+						$aDescription = $a->getDescription();
+						$bDescription = $b->getDescription();
+						$aNum = 0;
+						$bNum = 0;
+						foreach($KEYWORDS as $k) {
+							if(strpos($aDescription, $k) === false);
+							else $aNum++;
+							if(strpos($bDescription, $k) === false);
+							else $bNum++;
+						}
+						return $aNum - $bNum;
+					}
+				);
 				break;	
 		}
 
