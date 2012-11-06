5e7c3ca5dadde93185875ff4ac52329a93533ed2
mcastagnolo <mcastagnolo@mcastagnolo-PC>
2012-11-06 00:25:29 -0800

    Minor changes

--- scrapers/monster/MonsterScraper.php
+++ scrapers/monster/MonsterScraper.php
@@ -21,6 +21,7 @@ class MonsterScraper {
 
         // Create DOM from URL or file
         $ckfile = "cookies.txt";
+        //This should probably be changed to something dynamic.
         $useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"; // Sets user agent to iphone
 
         $ch = curl_init($url);
