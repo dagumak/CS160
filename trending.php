818611520998573241d7e67ce3938ef4abe7194c
mcastagnolo <mcastagnolo@mcastagnolo-PC>
2012-11-06 01:07:52 -0800

    Added a logging feature that logs search terms to the search_terms table.

--- trending.php
+++ trending.php
@@ -1,12 +1,12 @@
 <?php
 
-function log_search_terms($val) {
+function log_search_term($term) {
     $mysqli = get_job_lube_db_conn();
     $log_terms = $mysqli->query("INSERT INTO search_log (term)
-                        VALUES ('$val')");
+                        VALUES ('$term')");
 }
 
-function get_search_terms() {
+function get_top_search_terms() {
     $mysqli = get_job_lube_db_conn();
     $terms = $mysqli->query("SELECT distinct(term), 
         count(term) FROM `search_log` where 
