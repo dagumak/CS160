e3d4e7c066c65363c99251f5b8f579724a1ddcf6
Douglas Mak <github@douglasmak.com>
2012-11-10 22:04:23 -0800

    testing deploy script

--- index.php
+++ index.php
@@ -60,7 +60,6 @@ require_once 'database/db_util.php';
 		<script>
 			// Twitter
             !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-
             
 			function getResults() {
  				var sort = ''; 	
