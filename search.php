e1dff5e944690434afaee6acfc0239c85da7639b
Douglas Mak <github@douglasmak.com>
2012-11-15 11:39:06 -0800

    moved twitter js back to original spot

--- search.php
+++ search.php
@@ -112,6 +112,9 @@ if (isset($_GET["location"]) && isset($_GET["description"])) {
     }
     echo '</tbody> 
         </table> 
+        <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+        </script>
         </div>';
 }
 
